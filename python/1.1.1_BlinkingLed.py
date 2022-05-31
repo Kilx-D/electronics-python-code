8637df5f0a2dc68f4ba6fefde3a8bcf00b4bc6d1
krypton <kelxwii@gmail.com>
2022-05-31 19:13:15 -0400

    added more led

--- python/1.1.1_BlinkingLed.py
+++ python/1.1.1_BlinkingLed.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
-LedPin = 17
+
+LedPin = 21
+ledpin2 = 26
+ledpin3 = 16
+ledpin4 = 20
 def setup():
     # Set the GPIO modes to BCM Numbering
     GPIO.setmode(GPIO.BCM)
     # Set LedPin's mode to output,and initial level to High(3.3v)
+    
     GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(ledpin2, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(ledpin3, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(ledpin4, GPIO.OUT, initial=GPIO.HIGH)
 # Define a main function for main process
 def main():
     while True:
         print ('...LED ON')
         # Turn on LED
+        
         GPIO.output(LedPin, GPIO.LOW)
-        time.sleep(0.5)
-        print ('LED OFF...')
-        # Turn off LED
-        GPIO.output(LedPin, GPIO.HIGH)
-        time.sleep(0.5)
+        GPIO.output(ledpin2, GPIO.LOW)
+        GPIO.output(ledpin3, GPIO.LOW)
+        GPIO.output(ledpin4, GPIO.LOW)
+        # time.sleep(0.5)
+        # print ('LED OFF...')
+        # # Turn off LED
+        # GPIO.output(LedPin, GPIO.HIGH)
+        # time.sleep(0.5)
 # Define a destroy function for clean up everything after the script finished
 def destroy():
     # Turn off LED
